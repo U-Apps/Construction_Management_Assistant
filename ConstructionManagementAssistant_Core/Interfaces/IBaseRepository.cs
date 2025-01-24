ac5c337a5c557818f9d3c1cfcfcca3a07f6b3769
Saleh Bin-Sumida <salhbnsmyd3@gmail.com>
2025-01-25 01:18:54 +0300

    Add IBaseRepository<T> interface and BaseRepository<T> class
    Introduce IBaseRepository<T> interface for async data operations.
    Implement BaseRepository<T> class using AppDbContext to perform
    CRUD operations and other common data tasks.

---
+++ ConstructionManagementAssistant_Core/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace ConstructionManagementAssistant_Core.Interfaces;
+
+public interface IBaseRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync(string[] includes = null);
+
+    Task<T> GetByIdAsync(int Id, string[] includes = null);
+    Task<T> FindAsync(Expression<Func<T, bool>> predicate, string[] includes = null);
+    Task<IEnumerable<T>> FindAll(Expression<Func<T, bool>> predicate, string[] includes = null);
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+    Task AddAsync(T item);
+    Task UpdateAsync(T item);
+    Task DeleteAsync(int Id);
+
+}
---
+++ ConstructionManagementAssistant_EF/Repositories/BaseRepository.cs
@@ -0,0 +1,70 @@
+﻿using ConstructionManagementAssistant_Core.Interfaces;
+using ConstructionManagementAssistant_EF.Data;
+using System.Linq.Expressions;
+
+
+namespace RepositoryWithUWO.EF.Repositories;
+
+public class BaseRepository<T>(AppDbContext appDbContext) : IBaseRepository<T> where T : class
+{
+    private AppDbContext _appDbContext = appDbContext;
+
+    public async Task<T> GetByIdAsync(int id, string[] includes = null)
+    {
+        var entity = await _appDbContext.Set<T>().FindAsync(id);
+        return entity;
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync(string[] includes = null)
+    {
+        IQueryable<T> values = _appDbContext.Set<T>();
+
+        if (includes != null)
+        {
+            foreach (var include in includes)
+            {
+                values = values.Include(include);
+            }
+        }
+
+        return await values.ToListAsync();
+    }
+
+    public async Task<T> FindAsync(Expression<Func<T, bool>> predicate, string[] includes = null)
+    {
+        IQueryable<T> values = _appDbContext.Set<T>();
+
+        if (includes != null)
+        {
+            foreach (var include in includes)
+            {
+                values = values.Include(include);
+            }
+        }
+        return await values.SingleOrDefaultAsync(predicate);
+    }
+
+    public async Task<IEnumerable<T>> FindAll(Expression<Func<T, bool>> predicate, string[] includes = null)
+    {
+        IQueryable<T> values = _appDbContext.Set<T>();
+
+        if (includes != null)
+        {
+            foreach (var include in includes)
+            {
+                values = values.Include(include);
+            }
+        }
+        return await values.Where(predicate).ToListAsync();
+    }
+
+    // AnyAscync
+    public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate) => await _appDbContext.Set<T>().AnyAsync(predicate);
+
+    public async Task AddAsync(T item) => await _appDbContext.Set<T>().AddAsync(item);
+
+    public async Task UpdateAsync(T item) => _appDbContext.Set<T>().Update(item);
+
+    public async Task DeleteAsync(int id) => _appDbContext.Remove(await _appDbContext.FindAsync<T>(id));
+
+}
