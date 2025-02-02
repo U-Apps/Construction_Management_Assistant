1c902c80153d0b60cfae9650bf9806e72686697c
Yousef-Binnob <yusufbinnob7111@gmail.com>
2025-02-02 14:26:54 +0300

    Added WorkerSpecialty entity
    - Added WorkerSpecialty class to hold the name and id of the specialty
    - Add WorkerSpecialtyConfiguration to hold the EF configurations for the WorkerSpecialty entity

---
+++ ConstructionManagementAssistant_Core/Entites/WorkerSpecialty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstructionManagementAssistant_Core.Entites
+{
+    public class WorkerSpecialty : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? ModifiedDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
---
+++ ConstructionManagementAssistant_EF/Data/EntitesConfiguration/WorkerSpecialtyConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using ConstructionManagementAssistant_Core.Entites;
+
+namespace ConstructionManagementAssistant_EF.Configurations;
+internal class WorkerSpecialtyConfiguration : IEntityTypeConfiguration<WorkerSpecialty>
+{
+    public void Configure(EntityTypeBuilder<WorkerSpecialty> builder)
+    {
+        builder.ToTable("WorkerSpecialties");
+
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Name)
+        .HasColumnName("Name")
+        .HasMaxLength(100)
+        .IsUnicode(true)
+        .IsRequired(true);
+
+        builder.HasIndex(e => e.Name, "UniqueSpecialtyName")
+        .IsUnique(true);
+
+        builder.HasQueryFilter(e => !e.IsDeleted);
+
+    }
+}
