724bbde11262ca6560ea03560cf3dad49cbec112
Yousef-Binnob <yusufbinnob7111@gmail.com>
2025-02-02 18:58:24 +0300

    Added Worker entity
    - Added Worker class that inherits from Person
    - Add WorkerConfiguration to hold the EF configurations for the WorkerSpecialty entity

---
+++ ConstructionManagementAssistant_Core/Entites/Worker.cs
@@ -0,0 +1,10 @@
+﻿using ConstructionManagementAssistant_Core.Entites;
+
+namespace ConstructionManagementAssistant_Core;
+public class Worker : Person, IEntity
+{
+    public bool IsAvailable { get; set; }
+    public int? SpecialtyId { get; set; }
+
+    public WorkerSpecialty? Specialty { get; set; }
+}
