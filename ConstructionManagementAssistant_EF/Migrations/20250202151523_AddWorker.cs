1c3511b458ed75f3d66bcf6302210db9f2c7034f
Yousef-Binnob <yusufbinnob7111@gmail.com>
2025-02-03 00:02:27 +0300

    [migration] added Worker entity

---
+++ ConstructionManagementAssistant_EF/Migrations/20250202151523_AddWorker.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConstructionManagementAssistant_EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWorker : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Workers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false),
+                    SpecialtyId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Workers_People_Id",
+                        column: x => x.Id,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Workers_WorkerSpecialties_SpecialtyId",
+                        column: x => x.SpecialtyId,
+                        principalTable: "WorkerSpecialties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workers_SpecialtyId",
+                table: "Workers",
+                column: "SpecialtyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Workers");
+        }
+    }
+}
