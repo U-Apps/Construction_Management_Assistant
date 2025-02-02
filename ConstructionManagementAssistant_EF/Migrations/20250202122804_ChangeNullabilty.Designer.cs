520ddf7e3cdc13fb09415dccb3a884767ebd2788
Yousef-Binnob <yusufbinnob7111@gmail.com>
2025-02-02 15:30:24 +0300

    [migration] Added migration for person modification

--- ConstructionManagementAssistant_EF/Migrations/20250202122804_ChangeNullabilty.Designer.cs
+++ ConstructionManagementAssistant_EF/Migrations/20250202122804_ChangeNullabilty.Designer.cs
@@ -4,6 +4,7 @@ using ConstructionManagementAssistant_EF.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ConstructionManagementAssistant_EF.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250202122804_ChangeNullabilty")]
+    partial class ChangeNullabilty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
