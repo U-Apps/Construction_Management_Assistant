4c726ee42f085481cf9524fddcca3b376c45b556
Yousef-Binnob <yusufbinnob7111@gmail.com>
2025-02-02 23:04:50 +0300

    Add filters
    - changed signature of the GetAllWorkers method in the IWorkerRepository to accept filters
    - implemented filters in the GetAllWorkers method in the WorkerRepository

--- ConstructionManagementAssistant_Core/Interfaces/IWorkerRepository.cs
+++ ConstructionManagementAssistant_Core/Interfaces/IWorkerRepository.cs
@@ -7,8 +7,8 @@ namespace ConstructionManagementAssistant_Core.Interfaces
     public interface IWorkerRepository : IBaseRepository<Worker>
     {
         public Task<BaseResponse<string>> AddWorkerAsync(AddWorkerDto workerInfo);
-        public Task<PagedResult<GetWorkerDto>> GetAllWorkers(int pageNumber = 1, int pageSize = 10);
         public Task<BaseResponse<string>> UpdateWorkerAsync(UpdateWorkerDto workerInfo);
         public Task<BaseResponse<string>> DeleteWorkerAsync(int id);
+        Task<PagedResult<GetWorkerDto>> GetAllWorkers(int pageNumber = 1, int pageSize = 10, string? searchTerm = null, bool? isAvailable = null, int? SpecialtyId = null);
     }
 }
