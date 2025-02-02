280674f99e614b3f0478becbddd06e7d29e6cc3b
Yousef-Binnob <yusufbinnob7111@gmail.com>
2025-02-02 14:50:56 +0300

    Prepare data access for WorkerSpecialty CRUD operations
    - Add WorkerSpecialtyDtos
    - Add IWorkerSpecialtyRepository to abstract the repository
    - Add WorkerSpecialtyRepository that implement theIWorkerSpecialtyRepository interface

---
+++ ConstructionManagementAssistant_Core/DTOs/WorkerSpecialtyDtos.cs
@@ -0,0 +1,19 @@
+﻿namespace ConstructionManagementAssistant_Core.DTOs
+{
+    public class GetWorkerSpecialtyDto
+    {
+        public int Id { get; init; }
+        public string SpecialtyName { get; init; }
+    }
+
+    public class AddWorkerSpecialtyDto
+    {
+        public required string SpecialtyName { get; init; }
+    }
+
+    public class UpdateWorkerSpecialtyDto
+    {
+        public required int Id { get; init; }
+        public required string SpecialtyName { get; init; }
+    }
+}
