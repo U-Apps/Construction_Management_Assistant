1e7c1d578549bf6ded6296ade709b87ca6086257
Yousef-Binnob <yusufbinnob7111@gmail.com>
2025-02-02 18:58:24 +0300

    Added Worker entity
    - Added Worker class that inherits from Person
    - Add WorkerConfiguration to hold the EF configurations for the WorkerSpecialty entity

---
+++ ConstructionManagementAssistant_EF/Data/EntitesConfiguration/WorkerConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using ConstructionManagementAssistant_Core;
+using ConstructionManagementAssistant_Core.Entites;
+
+namespace ConstructionManagementAssistant_EF.Configurations;
+
+public class WorkerConfiguration : IEntityTypeConfiguration<Worker>
+{
+    public void Configure(EntityTypeBuilder<Worker> builder)
+    {
+        builder.ToTable("Workers");
+
+        builder.Property(w => w.IsAvailable)
+        .HasColumnName("IsAvailable")
+        .IsRequired(true);
+
+        builder.Property(w => w.SpecialtyId)
+        .HasColumnName("SpecialtyId")
+        .IsRequired(false);
+
+        builder.HasOne(w => w.Specialty)
+        .WithMany()
+        .HasForeignKey(w => w.SpecialtyId)
+        .IsRequired(false)
+        .OnDelete(DeleteBehavior.SetNull);
+    }
+}
