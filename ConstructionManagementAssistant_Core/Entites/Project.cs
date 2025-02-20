076bde2bd99d0b18daada430be71688e9be2d39f
faroog25 <faroogbajahaw25@gmail.com>
2025-02-20 14:27:29 +0300

    feature:add Project entity and ProjectStatus enum wit EF Core Configuration

---
+++ ConstructionManagementAssistant_Core/Entites/Project.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace ConstructionManagementAssistant_Core.Entites;
+public class Project
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateOnly Startdate { get; set; }
+    public DateOnly ExpectedEndDate { get; set; }
+    public ProjectStatus Status { get; set; }
+    public int SiteEngineerId { get; set; }
+    public int ClientId { get; set; }
+    public string? SiteAddress { get; set; }
+    public string? GeographicalCoordinates { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateOnly DateModified { get; set; }
+    public DateOnly DateCreated { get; set; }
+    public string? CancelationReason { get; set; }
+    public DateOnly? CancelationDate { get; set; }
+    public int? CancelledAtStage { get; set; }
+    public DateOnly? CompletionDate { get; set; }
+    public DateOnly? HandoverDate { get; set; }
+    public SiteEngineer SiteEngineer { get; set; }
+    public Client Client { get; set; }
+}
---
+++ ConstructionManagementAssistant_EF/Data/EntitesConfiguration/ProjectConfiguration.cs
@@ -0,0 +1,77 @@
+﻿using ConstructionManagementAssistant_Core.Enums;
+using ConstructionManagementAssistant_EF.Extensions;
+
+namespace ConstructionManagementAssistant_EF.Data.Configuration
+{
+    internal class ProjectConfiguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.ToTable("Projects");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(1000);
+
+            builder.Property(p => p.SiteAddress)
+                .IsRequired(false)
+                .HasMaxLength(500);
+
+            builder.Property(p => p.GeographicalCoordinates)
+                .IsRequired(false)
+                .HasMaxLength(100);
+
+            builder.Property(p => p.Status)
+                .IsRequired();
+
+            builder.Property(p => p.SiteEngineerId)
+                .IsRequired();
+
+            builder.Property(p => p.ClientId)
+                .IsRequired();
+
+            builder.Property(p => p.IsDeleted)
+                .HasDefaultValue(false);
+
+            builder.Property(p => p.DateModified)
+                .HasDefaultValueSql("CURRENT_DATE");
+
+            builder.Property(p => p.DateCreated)
+                .HasDefaultValueSql("CURRENT_DATE");
+
+            builder.Property(p => p.Startdate)
+                .IsRequired();
+
+            builder.Property(p => p.ExpectedEndDate)
+                .IsRequired();
+
+            builder.Property(p => p.CancelationReason)
+                .HasMaxLength(500);
+
+            // Nullable properties
+            builder.Property(p => p.CancelationDate);
+            builder.Property(p => p.CompletionDate);
+            builder.Property(p => p.HandoverDate);
+            builder.Property(p => p.CancelledAtStage);
+
+            builder.HasOne<Client>()
+            .WithOne()
+            .HasForeignKey<Project>(p => p.ClientId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne<SiteEngineer>()
+                .WithOne()
+                .HasForeignKey<Project>(p => p.SiteEngineerId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.AddEnumCheckConstraint<ProjectStatus>("Projects", "Status");
+
+            builder.HasQueryFilter(e => !e.IsDeleted);
+        }
+    }
+}
