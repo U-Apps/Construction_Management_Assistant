5e9d176b2570123f5436ea28d5f98b64dbd9ccaf
faroog25 <faroogbajahaw25@gmail.com>
2025-02-20 14:27:29 +0300

    feature:add Project entity and ProjectStatus enum wit EF Core Configuration

---
+++ ConstructionManagementAssistant_Core/Enums/ProjectStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace ConstructionManagementAssistant_Core.Enums;
+
+public enum ProjectStatus
+{
+    [Display(Name = "لم يبدأ")] NotStarted,
+    [Display(Name = "قيد التنفيذ")] InProgress,
+    [Display(Name = "معلق")] OnHold,
+    [Display(Name = "مكتمل")] Completed,
+    [Display(Name = "ملغي")] Cancelled
+}
