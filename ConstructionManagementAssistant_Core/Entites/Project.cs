f3d524aa5e4aa512b69227838856e1a8f287d92d
faroog25 <faroogbajahaw25@gmail.com>
2025-02-21 23:49:50 +0300

    fix: Implement IEntity interface and modify some properties

--- ConstructionManagementAssistant_Core/Entites/Project.cs
+++ ConstructionManagementAssistant_Core/Entites/Project.cs
@@ -1,12 +1,12 @@
 ﻿
 
 namespace ConstructionManagementAssistant_Core.Entites;
-public class Project
+public class Project:IEntity
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public DateOnly Startdate { get; set; }
+    public DateOnly StartDate { get; set; }
     public DateOnly ExpectedEndDate { get; set; }
     public ProjectStatus Status { get; set; }
     public int SiteEngineerId { get; set; }
@@ -14,8 +14,6 @@ public class Project
     public string? SiteAddress { get; set; }
     public string? GeographicalCoordinates { get; set; }
     public bool IsDeleted { get; set; }
-    public DateOnly DateModified { get; set; }
-    public DateOnly DateCreated { get; set; }
     public string? CancelationReason { get; set; }
     public DateOnly? CancelationDate { get; set; }
     public int? CancelledAtStage { get; set; }
@@ -23,4 +21,6 @@ public class Project
     public DateOnly? HandoverDate { get; set; }
     public SiteEngineer SiteEngineer { get; set; }
     public Client Client { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? ModifiedDate { get; set; }
 }
