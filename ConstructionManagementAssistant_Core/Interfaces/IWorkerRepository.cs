d9395b5f41516f900d633dcb656bb0e105dadde7
Saleh Bin-Sumida <salhbnsmyd3@gmail.com>
2025-02-23 18:58:39 +0300

    Refactor Worker repository and interface methods
    Updated `IWorkerRepository` to streamline worker retrieval and management methods.
    Refactored `WorkerRepository` to improve constructor usage, implement filtering in `GetAllWorkers`, and simplify worker addition, update, and deletion processes.
    Enhanced duplicate checks and error handling for better reliability.

--- ConstructionManagementAssistant_Core/Interfaces/IWorkerRepository.cs
+++ ConstructionManagementAssistant_Core/Interfaces/IWorkerRepository.cs
@@ -1,15 +1,18 @@
 ﻿using ConstructionManagementAssistant_Core.DTOs;
-using ConstructionManagementAssistant_Core.Entites;
 using ConstructionManagementAssistant_Core.Models.Response;
 
 namespace ConstructionManagementAssistant_Core.Interfaces
 {
     public interface IWorkerRepository : IBaseRepository<Worker>
     {
+        Task<PagedResult<GetWorkerDto>> GetAllWorkers(
+            int pageNumber,
+            int pageSize,
+            string? searchTerm = null,
+            bool? isAvailable = null);
+        Task<WorkerDetailsDto> GetWorkerById(int id);
         public Task<BaseResponse<string>> AddWorkerAsync(AddWorkerDto workerInfo);
         public Task<BaseResponse<string>> UpdateWorkerAsync(UpdateWorkerDto workerInfo);
         public Task<BaseResponse<string>> DeleteWorkerAsync(int id);
-        Task<PagedResult<GetWorkerDto>> GetAllWorkers(int pageNumber = 1, int pageSize = 10, string? searchTerm = null, bool? isAvailable = null, int? SpecialtyId = null);
-        Task<WorkerDetailsDto> GetWorkerById(int id);
     }
 }
--- ConstructionManagementAssistant_EF/Repositories/WorkerRepository.cs
+++ ConstructionManagementAssistant_EF/Repositories/WorkerRepository.cs
@@ -1,113 +1,65 @@
 ﻿using ConstructionManagementAssistant_Core.Mapping;
+using ConstructionManagementAssistant_EF.Extensions;
 
-namespace ConstructionManagementAssistant_EF.Repositories;
-
-public class WorkerRepository : BaseRepository<Worker>, IWorkerRepository
+namespace ConstructionManagementAssistant_EF.Repositories
 {
-    protected readonly AppDbContext _appDbContext;
-    public WorkerRepository(AppDbContext appDbContext) : base(appDbContext)
+    public class WorkerRepository(AppDbContext _context) : BaseRepository<Worker>(_context), IWorkerRepository
     {
-        _appDbContext = appDbContext;
-    }
 
-
-    public async Task<PagedResult<GetWorkerDto>> GetAllWorkers(int pageNumber = 1,
-                                                               int pageSize = 10,
-                                                               string? searchTerm = null,
-                                                               bool? isAvailable = null,
-                                                               int? SpecialtyId = null)
-    {
-        var query = _appDbContext.Set<Worker>()
-            .Include(w => w.Specialty)
-            .AsNoTracking();
-
-        if (!string.IsNullOrEmpty(searchTerm))
-            query = query.Where(w => w.FirstName.Contains(searchTerm)
-            || w.SecondName.Contains(searchTerm)
-            || w.ThirdName.Contains(searchTerm)
-            || w.LastName.Contains(searchTerm)
-            || w.Email.Contains(searchTerm)
-            || w.PhoneNumber.Contains(searchTerm)
-            || w.Address.Contains(searchTerm));
-
-
-        if (isAvailable.HasValue)
-            query = query.Where(w => w.IsAvailable == isAvailable.Value);
-
-        if (SpecialtyId.HasValue)
+        public async Task<PagedResult<GetWorkerDto>> GetAllWorkers(
+            int pageNumber = 1,
+            int pageSize = 10,
+            string? searchTerm = null,
+            bool? isAvailable = null)
         {
-            query = query.Where(w => w.SpecialtyId == SpecialtyId.Value);
+            Expression<Func<Worker, bool>> filter = x => true;
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                filter = filter.AndAlso(w =>
+                    w.FirstName.Contains(searchTerm) ||
+                    w.SecondName.Contains(searchTerm) ||
+                    w.ThirdName.Contains(searchTerm) ||
+                    w.LastName.Contains(searchTerm) ||
+                    w.Email.Contains(searchTerm) ||
+                    w.PhoneNumber.Contains(searchTerm) ||
+                    w.Address.Contains(searchTerm));
+            }
+
+            if (isAvailable.HasValue)
+                filter = filter.AndAlso(w => w.IsAvailable == isAvailable.Value);
+
+
+            var pagedResult = await GetPagedDataWithSelectionAsync(
+                orderBy: x => x.FirstName,
+                selector: WorkerProfile.ToGetWorkerDto(),
+                criteria: filter,
+                pageNumber: pageNumber,
+                pageSize: pageSize);
+
+            return pagedResult;
         }
 
-
-        var totalItems = await query.CountAsync();
-        var workers = await query
-            .OrderBy(w => w.FirstName)
-            .Skip((pageNumber - 1) * pageSize)
-            .Take(pageSize)
-            .Select(w => w.ToGetWorkerDto())
-            .ToListAsync();
-
-        return new PagedResult<GetWorkerDto>
+        public async Task<WorkerDetailsDto> GetWorkerById(int id)
         {
-            Items = workers,
-            TotalCount = totalItems,
-            TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize),
-            CurrentPage = pageNumber,
-            PageSize = pageSize
-        };
-    }
+            return await FindWithSelectionAsync(
+                selector: WorkerProfile.ToWorkerDetailsDto(),
+                criteria: x => x.Id == id);
+        }
 
-    public async Task<WorkerDetailsDto> GetWorkerById(int id)
-    {
-        var query = await _appDbContext.Set<Worker>()
-                                       .Include(w => w.Specialty)
-                                       .Where(x => x.Id == id)
-                                       .Select(w => w.ToWorkerDetailsDto())
-                                       .SingleOrDefaultAsync();
-
-        return query;
-    }
-
-
-    public async Task<BaseResponse<string>> AddWorkerAsync(AddWorkerDto workerInfo)
-    {
-        try
+        public async Task<BaseResponse<string>> AddWorkerAsync(AddWorkerDto workerDto)
         {
-            var emailExists = await _appDbContext.Set<Worker>().AnyAsync(c => c.Email != null && c.Email == workerInfo.Email);
-            if (emailExists)
-            {
-                return new BaseResponse<string>
-                {
-                    Success = false,
-                    Message = "البريد الإلكتروني موجود بالفعل",
-                };
-            }
+            var duplicateCheck = await CheckDuplicatePhoneEmailNationalNumberAsync(
+                phoneNumber: workerDto.PhoneNumber,
+                email: workerDto.Email,
+                nationalNumber: workerDto.NationalNumber);
 
-            var phoneExists = await _appDbContext.Set<Worker>().AnyAsync(c => c.PhoneNumber != null && c.PhoneNumber == workerInfo.PhoneNumber);
-            if (phoneExists)
-            {
-                return new BaseResponse<string>
-                {
-                    Success = false,
-                    Message = "رقم الهاتف موجود بالفعل",
-                };
-            }
+            if (!duplicateCheck.Success)
+                return duplicateCheck;
 
-            var NationalNumberExists = await _appDbContext.Set<Worker>().AnyAsync(c => c.NationalNumber != null && c.NationalNumber == workerInfo.NationalNumber);
-            if (phoneExists)
-            {
-                return new BaseResponse<string>
-                {
-                    Success = false,
-                    Message = "رقم الهوية موجود بالفعل"
-                };
-            }
-
-            var newWorker = workerInfo.ToWorker();
-
-            await _appDbContext.AddAsync(newWorker);
-            await _appDbContext.SaveChangesAsync();
+            var newWorker = workerDto.ToWorker();
+            await AddAsync(newWorker);
+            await _context.SaveChangesAsync();
 
             return new BaseResponse<string>
             {
@@ -115,65 +67,30 @@ public class WorkerRepository : BaseRepository<Worker>, IWorkerRepository
                 Message = "تم إضافة العامل بنجاح"
             };
         }
-        catch (Exception)
+
+        public async Task<BaseResponse<string>> UpdateWorkerAsync(UpdateWorkerDto workerInfo)
         {
-            return new BaseResponse<string>
-            {
-                Success = false,
-                Message = "لم تتم إضافة العامل ",
-                Errors = ["حصل خطأ"]
-            };
-        }
+            var worker = await GetByIdAsync(workerInfo.Id);
 
-    }
+            if (worker is null)
+                return new BaseResponse<string>
+                {
+                    Success = false,
+                    Message = "العامل غير موجود"
+                };
 
 
-    public async Task<BaseResponse<string>> UpdateWorkerAsync(UpdateWorkerDto workerInfo)
-    {
-        var worker = await _appDbContext.Set<Worker>().Where(w => w.Id == workerInfo.Id).FirstOrDefaultAsync();
-        if (worker is null)
-            return new BaseResponse<string>
-            {
-                Success = false,
-                Message = "العامل غير موجود"
-            };
+            var duplicateCheck = await CheckDuplicatePhoneEmailNationalNumberAsync(
+                phoneNumber: workerInfo.PhoneNumber,
+                email: workerInfo.Email,
+                nationalNumber: workerInfo.NationalNumber,
+                id: workerInfo.Id);
 
-        var emailExists = await _appDbContext.Set<Worker>().AnyAsync(c => c.Email != null && c.Email == workerInfo.Email);
-        if (emailExists && workerInfo.Email != worker.Email)
-        {
-            return new BaseResponse<string>
-            {
-                Success = false,
-                Message = "البريد الإلكتروني موجود بالفعل"
-            };
-        }
+            if (!duplicateCheck.Success)
+                return duplicateCheck;
 
-        var phoneExists = await _appDbContext.Set<Worker>().AnyAsync(c => c.PhoneNumber != null && c.PhoneNumber == workerInfo.PhoneNumber);
-        if (phoneExists && workerInfo.PhoneNumber != worker.PhoneNumber)
-        {
-            return new BaseResponse<string>
-            {
-                Success = false,
-                Message = "رقم الهاتف موجود بالفعل"
-            };
-        }
-
-        var NationalNumberExists = await _appDbContext.Set<Worker>().AnyAsync(c => c.NationalNumber != null && c.NationalNumber == workerInfo.NationalNumber);
-        if (NationalNumberExists && workerInfo.NationalNumber != worker.NationalNumber)
-        {
-            return new BaseResponse<string>
-            {
-                Success = false,
-                Message = "رقم الهوية موجود بالفعل"
-            };
-        }
-        try
-        {
-            workerInfo.MapToWorker(worker);
-            
-
-            _appDbContext.Update(worker);
-            await _appDbContext.SaveChangesAsync();
+            workerInfo.UpdateWorker(worker);
+            await _context.SaveChangesAsync();
 
             return new BaseResponse<string>
             {
@@ -181,33 +98,25 @@ public class WorkerRepository : BaseRepository<Worker>, IWorkerRepository
                 Message = "تم تحديث العامل بنجاح"
             };
         }
-        catch (Exception)
+
+        public async Task<BaseResponse<string>> DeleteWorkerAsync(int id)
         {
+            var worker = await GetByIdAsync(id);
+            if (worker is null)
+                return new BaseResponse<string>
+                {
+                    Success = false,
+                    Message = "العامل غير موجود"
+                };
+
+            Delete(worker);
+            await _context.SaveChangesAsync();
+
             return new BaseResponse<string>
             {
-                Success = false,
-                Message = "لم تتم عميلة التعديل",
-                Errors = ["حصل خطأ"]
+                Success = true,
+                Message = "تم حذف العامل بنجاح"
             };
         }
-    }
-
-    public async Task<BaseResponse<string>> DeleteWorkerAsync(int id)
-    {
-        var worker = _appDbContext.Set<Worker>().Where(x => x.Id == id).FirstOrDefault();
-        if (worker is null)
-            return new BaseResponse<string>
-            {
-                Success = false,
-                Message = "العامل غير موجود"
-            };
-
-        worker.IsDeleted = true;
-        await _appDbContext.SaveChangesAsync();
-        return new BaseResponse<string>
-        {
-            Success = true,
-            Message = "تم حذف العامل بنجاح"
-        };
     }
 }
