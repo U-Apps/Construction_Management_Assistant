c39b55783ca39bada1d01b181ddb573e347563d1
faroog25 <faroogbajahaw25@gmail.com>
2025-03-04 13:16:29 +0300

    feat: Add mapping methods to ProjectProfile
    - Added ToGetProjectDto method to map Project to GetProjectsDto.
    - Added ToProject extension method to map AddProjectDto to Project.
    - Added UpdateProject extension method to update Project from UpdateProjectDto.

---
+++ ConstructionManagementAssistant_Core/Mapping/ProjectProfile.cs
@@ -0,0 +1,45 @@
+﻿using ConstructionManagementAssistant_Core.DTOs;
+using ConstructionManagementAssistant_Core.Entites;
+using ConstructionManagementAssistant_Core.Extentions;
+using System.Linq.Expressions;
+
+namespace ConstructionManagementAssistant_Core.Mapping;
+
+public static class ProjectProfile
+{
+    public static Expression<Func<Project, GetProjectsDto>> ToGetProjectDto()
+    {
+        return project => new GetProjectsDto
+        {
+            Id = project.Id,
+            ProjectName = project.Name,
+            SiteAddress = project.SiteAddress,
+            ClientName = project.Client.FullName,
+            ProjectStatus = (project.Status).GetDisplayName(),
+        };
+    }
+
+    public static Project ToProject(this AddProjectDto addProjectDto)
+    {
+        return new Project
+        {
+            Name=addProjectDto.ProjectName,
+            Description = addProjectDto.Description,
+            SiteAddress = addProjectDto.SiteAddress,
+            GeographicalCoordinates = addProjectDto.GeographicalCoordinates,
+            SiteEngineerId = addProjectDto.SiteEngineerId,
+            ClientId= addProjectDto.ClientId,
+            StartDate = addProjectDto.StartDate,
+            ExpectedEndDate = addProjectDto.ExpectedEndDate
+        };
+    }
+
+    public static void UpdateProject(this Project project, UpdateProjectDto updateProjectDto)
+    {
+        project.Name = updateProjectDto.ProjectName;
+        project.Description = updateProjectDto.Description;
+        project.SiteAddress = updateProjectDto.SiteAddress;
+        project.GeographicalCoordinates = updateProjectDto.GeographicalCoordinates;
+        project.ModifiedDate = DateTime.Now;
+    }
+}
