6761051cf138a7a18a78d4a1eab4bb45edd190db
faroog25 <faroogbajahaw25@gmail.com>
2025-03-04 12:54:08 +0300

    feat: Add methods to IProjectRepository interface
    - Added GetProjectById method to retrieve a project by its ID.
    - Added GetAllProjects method to retrieve all projects with pagination and optional search term.

---
+++ ConstructionManagementAssistant_Core/Interfaces/IProjectRepository.cs
@@ -0,0 +1,15 @@
+﻿using ConstructionManagementAssistant_Core.DTOs;
+using ConstructionManagementAssistant_Core.Entites;
+using ConstructionManagementAssistant_Core.Models.Response;
+
+namespace ConstructionManagementAssistant_Core.Interfaces
+{
+    public interface IProjectRepository : IBaseRepository<Project>
+    {
+        public Task<GetProjectDto> GetProjectById(int id);
+        public Task<PagedResult<GetProjectDto>> GetAllProjects(
+            int pageNumber,
+            int pageSize,
+            string? searchTerm = null );
+    }
+}
