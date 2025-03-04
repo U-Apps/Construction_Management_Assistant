aeca3668098c21ed3f82d6c6d4490978bc912d50
faroog25 <faroogbajahaw25@gmail.com>
2025-03-04 13:21:18 +0300

    feat: Implement ProjectRepository with GetProjectById method
    - Added GetProjectById method to retrieve a project by its ID using a selector.

---
+++ ConstructionManagementAssistant_EF/Repositories/ProjectRepository.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace ConstructionManagementAssistant_EF.Repositories;
+
+public class ProjectRepository(AppDbContext _context) : BaseRepository<Project>(_context), IProjectRepository
+{
+    public async Task<GetProjectsDto> GetProjectById(int id)
+    {
+        return await FindWithSelectionAsync(
+            selector: ProjectProfile.ToGetProjectDto(),
+            criteria: x => x.Id == id);
+    }
+
+   
+}
