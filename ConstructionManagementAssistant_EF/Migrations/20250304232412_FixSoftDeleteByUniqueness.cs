9f7466aa3a337e9a3bafbed2cdea2dbdb969a588
faroog25 <faroogbajahaw25@gmail.com>
2025-03-05 02:26:10 +0300

    [Migration] Implement soft delete functionality in EF Core models
    Updated migration files and model snapshot to enforce soft delete logic by adding filters to unique indexes. Ensured that only non-deleted records are considered for uniqueness constraints on `Email`, `PhoneNumber`, and `NationalNumber` fields across `Client`, `Person`, `Project`, `WorkerSpecialty`, `SiteEngineer`, and `Worker` entities. This enhances data integrity by preventing soft-deleted records from affecting uniqueness.

---
+++ ConstructionManagementAssistant_EF/Migrations/20250304232412_FixSoftDeleteByUniqueness.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConstructionManagementAssistant_EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSoftDeleteByUniqueness : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UniqueSpecialtyName",
+                table: "WorkerSpecialties");
+
+            migrationBuilder.DropIndex(
+                name: "UniqueEmail",
+                table: "People");
+
+            migrationBuilder.DropIndex(
+                name: "UniqueNationalNumber",
+                table: "People");
+
+            migrationBuilder.DropIndex(
+                name: "UniquePhoneNumber",
+                table: "People");
+
+            migrationBuilder.DropIndex(
+                name: "UniqueEmail",
+                table: "Clients");
+
+            migrationBuilder.DropIndex(
+                name: "UniquePhoneNumber",
+                table: "Clients");
+
+            migrationBuilder.CreateIndex(
+                name: "UniqueSpecialtyName",
+                table: "WorkerSpecialties",
+                column: "Name",
+                unique: true,
+                filter: "[IsDeleted] = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "UniqueEmail",
+                table: "People",
+                column: "Email",
+                unique: true,
+                filter: "[IsDeleted] = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "UniqueNationalNumber",
+                table: "People",
+                column: "NationalNumber",
+                unique: true,
+                filter: "[IsDeleted] = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "UniquePhoneNumber",
+                table: "People",
+                column: "PhoneNumber",
+                unique: true,
+                filter: "[IsDeleted] = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "UniqueEmail",
+                table: "Clients",
+                column: "Email",
+                unique: true,
+                filter: "[IsDeleted] = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "UniquePhoneNumber",
+                table: "Clients",
+                column: "PhoneNumber",
+                unique: true,
+                filter: "[IsDeleted] = 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UniqueSpecialtyName",
+                table: "WorkerSpecialties");
+
+            migrationBuilder.DropIndex(
+                name: "UniqueEmail",
+                table: "People");
+
+            migrationBuilder.DropIndex(
+                name: "UniqueNationalNumber",
+                table: "People");
+
+            migrationBuilder.DropIndex(
+                name: "UniquePhoneNumber",
+                table: "People");
+
+            migrationBuilder.DropIndex(
+                name: "UniqueEmail",
+                table: "Clients");
+
+            migrationBuilder.DropIndex(
+                name: "UniquePhoneNumber",
+                table: "Clients");
+
+            migrationBuilder.CreateIndex(
+                name: "UniqueSpecialtyName",
+                table: "WorkerSpecialties",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UniqueEmail",
+                table: "People",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "UniqueNationalNumber",
+                table: "People",
+                column: "NationalNumber",
+                unique: true,
+                filter: "[NationalNumber] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "UniquePhoneNumber",
+                table: "People",
+                column: "PhoneNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UniqueEmail",
+                table: "Clients",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "UniquePhoneNumber",
+                table: "Clients",
+                column: "PhoneNumber",
+                unique: true);
+        }
+    }
+}
