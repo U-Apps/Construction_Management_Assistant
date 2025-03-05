43c82fca33dfa9b00d11655529904d8d0b2942f8
Yousef-Binnob <yusufbinnob7111@gmail.com>
2025-03-05 19:14:36 +0300

    Add Stage entity and update EF services
    - Added a new constant `Stages` to the `TablesNames` class in `TablesNames.cs`.
    - Updated `AppDbContextModelSnapshot.cs` to include the new `Stage` entity with its properties and configurations.
    - Introduced a new `Stage` class in `Stage.cs`.
    - Added a new `StageConfiguration` class in `StageConfiguration.cs` to configure the `Stage` entity.
    - Created a new migration for the Stage entity.

---
+++ ConstructionManagementAssistant_Core/Entites/Stage.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace ConstructionManagementAssistant_Core.Entites
+{
+    public class Stage : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public DateOnly? StartDate { get; set; }
+        public DateOnly? EndDate { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? ModifiedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ ConstructionManagementAssistant_EF/Data/EntitesConfiguration/StageConfiguration.cs
@@ -0,0 +1,24 @@
+﻿
+
+using ConstructionManagementAssistant_Core.Constants;
+
+namespace ConstructionManagementAssistant_EF.Data.EntitesConfiguration
+{
+    internal class StageConfiguration : IEntityTypeConfiguration<Stage>
+    {
+        public void Configure(EntityTypeBuilder<Stage> builder)
+        {
+            builder.ToTable(TablesNames.Stages);
+
+            builder.Property(s => s.Name)
+                .HasMaxLength(200);
+
+            builder.Property(s => s.Description)
+                .HasMaxLength(1000);
+
+            // Global filter to exclude deleted clients
+            builder.HasQueryFilter(e => !e.IsDeleted);
+
+        }
+    }
+}
