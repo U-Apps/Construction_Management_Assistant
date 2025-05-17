792cb75be78979e29c348f027bb447ed61bcb4f3
Yousef-Binnob <yusufbinnob7111@gmail.com>
2025-05-17 03:23:27 +0300

    added update document

---
+++ src/ConstructionManagementAssistant_Core/DTOs/UpdateDocumentRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionManagementAssistant.Core.DTOs
+{
+    public class UpdateDocumentRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
