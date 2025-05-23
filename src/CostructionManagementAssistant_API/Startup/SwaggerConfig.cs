85cdb6536ed0213351e62952c1094c2e2b7bc15d
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-24 01:10:50 +0300

    Add Bearer token authentication to Swagger config
    Implemented security definitions and requirements for Bearer token authentication in the Swagger configuration. This includes the specification of the token's name, type, scheme, and description, along with a security requirement referencing the defined security scheme.

--- src/CostructionManagementAssistant_API/Startup/SwaggerConfig.cs
+++ src/CostructionManagementAssistant_API/Startup/SwaggerConfig.cs
@@ -34,6 +34,33 @@ public static class SwaggerConfig
             var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
             var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
             c.IncludeXmlComments(xmlPath);
+
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
+
         });
 
         // Configure CORS
